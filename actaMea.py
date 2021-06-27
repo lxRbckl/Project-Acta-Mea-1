0214d8f7d3c145e49d303e23a2f6d1873c8e5a70
adrienhighlander <alexarbuckle@protonmail.com>
2021-06-27 11:02:17 -0500

    updated version: - jsonLoad, jsonDump added - setServer rebuilt to allow overwrites - showServer broken down for readability - removeServer added - aliases added

--- actaMea.py
+++ actaMea.py
@@ -8,93 +8,96 @@ from discord.ext.commands import Bot
 
 
 actaMea = Bot(command_prefix = '', intents = Intents.all())
-token = ''
+token = 'NzkxNjk5MTIzNzA3NDQ1MjQ4.X-S9RA.7TIB5z25XGMwG4bCch3RPpBZi8k'
 
 
-@actaMea.command()
-async def getServer(ctx, arg):
-    ''' arg : string, server name '''
-
-    # if user
-    if ('Germx5000' in str(ctx.author)[:-5]):
-
-        # load data
-        with open('actaMea.json', 'r') as fileVariable:
-
-            dictVariable = load(fileVariable)
-
-        # if server exists
-        if (arg in dictVariable.keys()):
-
-            await ctx.send(await actaMea.get_channel(dictVariable[arg]).create_invite(),
-                           delete_after = 60.0)
-
-        else:
-
-            await ctx.channel.send('{} does not exist.'.format(arg),
-                                   delete_after = 60.0)
-
-
-@actaMea.command()
-async def setServer(ctx, arg):
-    ''' arg : string, server name '''
-
-    # if user
-    if ('Germx5000' in str(ctx.author)[:-5]):
-
-        # load data
-        with open('actaMea.json', 'r') as fileVariable:
-
-            dictVariable = load(fileVariable)
-
-        # if server exists
-        if (int(ctx.channel.id) in dictVariable.values()):
-
-            await ctx.channel.send('{} already exists.'.format(arg),
-                                   delete_after = 60.0)
-
-        else:
-
-            # add server
-            dictVariable[arg] = int(ctx.channel.id)
-
-            await ctx.channel.send('{} was added.'.format(arg))
-
-        # dump data
-        with open('actaMea.json', 'w') as fileVariable:
-
-            dump(dictVariable,
-                 fileVariable,
-                 indent = 4)
-
-
-@actaMea.command()
-async def showServer(ctx):
+async def jsonLoad():
     '''  '''
 
-    # if user
-    if ('Germx5000' in str(ctx.author)[:-5]):
+    with open('actaMea.json', 'r') as fileVariable:
 
-        # load data
-        with open('actaMea.json', 'r') as fileVariable:
+        return load(fileVariable)
 
-            dictVariable = load(fileVariable)
 
-        # send all keys
-        await ctx.channel.send(''.join('{}\n'.format(i) for i in dictVariable.keys()),
-                               delete_after = 60.0)
+async def jsonDump(arg):
+    ''' arg : dict '''
+
+    with open('actaMea.json', 'w') as fileVariable:
+
+        dump(arg, fileVariable, indent = 4)
 
 
 @actaMea.event
 async def on_member_join(member):
-    '''  '''
+    ''' member : class '''
 
-    # if user
     if ('Germx5000' in str(member)[:-5]):
 
-        # add role
-        await member.add_roles(utils.get(member.guild.roles,
-                                         name = 'Admin'))
+        await member.add_roles(utils.get(member.guild.roles, name = 'Admin'))
 
 
-actaMea.run(token)
+@actaMea.command(aliases = ['get', 'Get'])
+async def getServer(ctx, arg):
+    ''' arg : str '''
+
+    if ('Germx5000' in str(ctx.author)[:-5]):
+
+        dictVariable = await jsonLoad()
+
+        if (arg in dictVariable.keys()):
+
+            await ctx.send(await actaMea.get_channel(dictVariable[arg]).create_invite(), delete_after = 60)
+
+        else:
+
+            await ctx.channel.send('{} does not exist.'.format(arg), delete_after = 60)
+
+
+@actaMea.command()
+async def setServer(ctx, arg):
+    ''' arg : str '''
+
+    if ('Germx5000' in str(ctx.author)[:-5]):
+
+        dictVariable = await jsonLoad()
+
+        if (int(ctx.channel.id) in dictVariable.values()):
+
+            del dictVariable[arg]
+
+        dictVariable[arg] = int(ctx.channel.id)
+        await jsonDump(dictVariable)
+
+        await ctx.channel.send('{} was added.'.format(arg))
+
+
+@actaMea.command(aliases = ['show', 'Show'])
+async def showServer(ctx):
+    '''  '''
+
+    if ('Germx5000' in str(ctx.author)[:-5]):
+
+        dictVariable = await jsonLoad()
+        strVariable = ''.join('{}\n'.format(i) for i in dictVariable.keys())
+
+        await ctx.channel.send(strVariable, delete_after = 60)
+
+
+@actaMea.command(aliases = ['remove', 'Remove'])
+async def removeServer(ctx, arg):
+    ''' arg : str '''
+
+    if ('Germx5000' in str(ctx.author)[:-5]):
+
+        dictVariable = await jsonLoad()
+
+        if (arg not in dictVariable.keys()):
+
+            await ctx.channel.send('{} does not exist.'.format(arg), delete_after = 60)
+
+        else:
+
+            del dictVariable[arg]
+            await jsonDump(dictVariable)
+
+            await ctx.channel.send('{} was removed.'.format(arg), delete_after = 60)
